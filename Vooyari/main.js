4691c6c223dd3f6aac9de005f77daf8910e8d18f
Vooyari <misha_gorbik@mail.ru>
2025-03-17 20:43:55 +0400

    Обновление файла main.js

---
+++ Vooyari/main.js
@@ -0,0 +1,31 @@
+let sec = 0;
+let clearTime;
+let run = true;
+
+const startPause = document.getElementById("start-pause");
+const clear = document.getElementById('stop');
+
+function stopwatch() {
+    if (run) {
+        clearTime = setInterval(() => {
+            sec++;
+            console.log(sec + ' секунда');
+        }, 1000);
+        run = false;
+    } else {
+        console.log('Пауза');
+        clearInterval(clearTime);
+        run = true;
+    };
+};
+function clearStopwatch() {
+    console.log('Сброс секундомера');
+    clearInterval(clearTime);
+    sec = 0;
+    run = true;
+};
+
+startPause.addEventListener('click', stopwatch);
+clear.addEventListener('click', clearStopwatch);
+
+//Приветик приветик
